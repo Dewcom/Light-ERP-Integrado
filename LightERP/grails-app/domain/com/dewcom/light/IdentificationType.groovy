8718dbe497da37fc22ae3a59ccac22f5380e8625
Leonardo Chen <lchen@Leonardos-MacBook-Pro.local>
2017-06-23 00:47:36 -0600

    changes

--- LightERP/grails-app/domain/com/dewcom/light/IdentificationType.groovy
+++ LightERP/grails-app/domain/com/dewcom/light/IdentificationType.groovy
@@ -3,6 +3,7 @@ package com.dewcom.light
 class IdentificationType {
     final static int COMPANY_CODE = 2
     final static int PHYSICAL_CODE = 1
+    final static int PASSPORT_CODE = 3
     String name
     Byte enabled = Constants.ESTADO_ACTIVO;
     Date registrationDate = new Date()
