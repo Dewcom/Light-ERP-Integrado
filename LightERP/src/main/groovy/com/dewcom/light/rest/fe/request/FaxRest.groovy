f05105ee203727c2029d7b4ccc2672b7cc6358a9
cdemoncr <chenmon.2092@gmail.com>
2018-10-16 09:21:02 -0600

    creacion factura electronica clases involcuradas base.

---
+++ LightERP/src/main/groovy/com/dewcom/light/rest/fe/request/FaxRest.groovy
@@ -0,0 +1,7 @@
+package com.dewcom.light.rest.fe.request
+
+/**
+ * Created by lchen on 10/2/18.
+ */
+class FaxRest {
+}
---
+++ LightERP/src/main/groovy/com/dewcom/light/rest/fe/request/invoice/InvoiceHeader.groovy
@@ -0,0 +1,8 @@
+package com.dewcom.light.rest.fe.request.invoice
+
+/**
+ * Created by lchen on 10/2/18.
+ */
+class InvoiceHeader {
+
+}
